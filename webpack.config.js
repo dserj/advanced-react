76ef8c92f9d4dffaeb280342a8af2754f30e4bd0
serj <>
2017-11-21 18:17:54 +0300

    added simple webpack

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+const config = {
+  entry: './lib/components/Index.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.(js|jsx)$/, use: 'babel-loader', exclude: /node_modules/ }
+    ]
+  }
+};
+
+module.exports = config;
